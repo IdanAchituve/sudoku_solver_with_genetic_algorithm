6fbc141501f724437a6b2fad0f1c4dda98060509
idan achituve <idanachi@gmail.com>
2019-04-12 22:28:41 +0300

    3rd

--- game_board.py
+++ game_board.py
@@ -5,13 +5,14 @@ np.random.seed(111)
 NUM_ROWS = NUM_COLS = 9
 MIN_VAL = 1
 MAX_VAL = 9
-
+MUTATION_RATE = 0.6
 
 # sum automaton class
 class candidate:
 
     def __init__(self, board):
-        self.board = board
+        self.board = board.copy()
+        self.fixed_board = board.copy()  # save the fixed cells of the board
         self.fitness = 0
         self.prob = 0
 
@@ -59,6 +60,28 @@ class candidate:
     def get_prob(self):
         return self.prob
 
-    def check_validity(self):
+    def get_board(self):
+        return self.board
 
-       xxx = 1
+    def set_board(self, board):
+        self.board = board
+
+    def mutate(self):
+
+        for row in range(NUM_ROWS):
+            # per each row generate random number and do mutation
+            rand_num = np.random.random()
+
+            # do mutation if there are at least to open cells
+            if rand_num < MUTATION_RATE and np.count_nonzero(self.fixed_board[row]) <= (MAX_VAL - 2):
+                # pick random cells in the row in vacant places
+                col1 = np.random.randint(0, NUM_COLS)
+                col2 = np.random.randint(0, NUM_COLS)
+                while self.fixed_board[row, col1] != 0 or self.fixed_board[row, col2] != 0 or col1 == col2:
+                    col1 = np.random.randint(0, NUM_COLS) if self.fixed_board[row, col1] != 0 else col1
+                    col2 = np.random.randint(0, NUM_COLS) if self.fixed_board[row, col2] != 0 or col2 == col1 else col2
+
+                # switch values
+                temp = self.board[row, col2]
+                self.board[row, col2] = self.board[row, col1]
+                self.board[row, col1] = temp
--- sudoku_solver.py
+++ sudoku_solver.py
@@ -1,22 +1,25 @@
 
 import numpy as np
 import operator
+import copy
+import datetime
 import game_board as gb
 
 
 np.random.seed(111)
 
 NUM_SOL = 100
-NUM_REPLICATIONS = 0.25 * NUM_SOL  # number of replications
+NUM_REPLICATIONS = 0.15 * NUM_SOL  # number of replications
 MUTATION_PERC = 0.05  # % of mutations
 NUM_ELITISM = 0.05 * NUM_SOL  # number of best solutions to take
+
 
 # generate initial solutions from user input
 def generate_initial_solutions(input_board):
 
     solutions = []
     for i in range(NUM_SOL):
-        board = gb.candidate(input_board)
+        board = gb.candidate(input_board.copy())
         board.draw_random_solution()
         solutions.append(board)
     return solutions
@@ -76,7 +79,7 @@ def from_fitness_to_probs(solutions, f_sum):
 
 
 # select solutions based on biased selection
-def select_solutions_for_replications(solutions):
+def bias_selection(solutions, sample_size=int(NUM_REPLICATIONS)):
 
     sol_probs = []  # save probability of each solution
     # get probabilities while relying on the fact that they are sorted
@@ -86,8 +89,89 @@ def select_solutions_for_replications(solutions):
     sol_indices = np.arange(NUM_SOL)  # indices of the solutions
 
     # sample solutions according to their probabilities without replacement
-    sampled_indices = np.random.choice(sol_indices, size=int(NUM_REPLICATIONS), replace=False, p=sol_probs)
+    sampled_indices = np.random.choice(sol_indices, size=sample_size, replace=False, p=sol_probs)
     return sorted(sampled_indices.tolist())
+
+
+# mix 2 parent rows according to: Learning Bayesian network structures by searching for the best ordering with genetic algorithms. IEEE Transactions on System, Man and Cybernetics, 26(4):487-493
+def cyclic_crossover(prow1, prow2):
+
+    # new row values
+    crow1 = np.zeros(gb.MAX_VAL)
+    crow2 = np.zeros(gb.MAX_VAL)
+
+    left = list(range(gb.MAX_VAL))
+    cycle = 0
+
+    for i in range(gb.MAX_VAL):
+
+        # already assigned this values
+        if i not in left:
+            continue
+
+        if cycle % 2 == 0:
+            start_val = prow1[i]  # value of the 1st parent at the ith position
+            p2val = prow2[i]  # value of the 2nd parent at the ith position
+            crow1[i] = start_val  # 1st child with 1st parent values at the ith position
+            crow2[i] = p2val  # 2nd child with the 2nd parent value at the uth position
+            left.remove(i)
+
+            while start_val != p2val:
+                p1index = np.where(prow1 == p2val)[0].item()
+                crow1[p1index] = prow1[p1index]  # get value in the 1st parent corresponding to the index equal to the value of the 2nd parent
+                p2val = prow2[p1index]
+                crow2[p1index] = p2val
+                left.remove(p1index)
+
+            cycle += 1
+
+        else:
+            start_val = prow1[i]  # value of the 1st parent at the ith position
+            p2val = prow2[i]  # value of the 2nd parent at the ith position
+            crow1[i] = p2val  # 1st child with 2nd parent values at the ith position
+            crow2[i] = start_val  # 2nd child with the 1st parent value at the uth position
+            left.remove(i)
+
+            while start_val != p2val:
+                p1index = np.where(prow1 == p2val)[0].item()
+                p2val = prow2[p1index]
+                crow1[p1index] = p2val  # get value in the 1st parent corresponding to the index equal to the value of the 2nd parent
+                crow2[p1index] = prow1[p1index]
+                left.remove(p1index)
+
+            cycle += 1
+
+    return crow1, crow2
+
+
+# perform cycle crossover on rows
+def cross_over(solutions, num_cross_over_sols):
+
+    cross_over_solutions = []
+    for i in range(int(num_cross_over_sols)):
+        parent_indices = bias_selection(solutions, 2)  # choose randomly 2 parents
+        parent1 = solutions[parent_indices[0]]  # get the 1st parent
+        parent2 = solutions[parent_indices[1]]  # get the 2nd parent
+        child1 = parent1.get_board().copy()
+        child2 = parent2.get_board().copy()
+
+        # select randomly a range of rows to perform crossover on
+        start_row = end_row = 0
+        while start_row >= end_row:
+            start_row = np.random.randint(0, gb.NUM_ROWS)
+            end_row = np.random.randint(0, gb.NUM_ROWS)
+
+        # mix row1 and row2 values so that each row will be a valid row (i.e., all values between 1-9)
+        for row in range(start_row, end_row):
+            child1[row, :], child2[row, :] = cyclic_crossover(child1[row, :], child2[row, :])
+
+        # update board for next generation
+        parent1.set_board(child1)
+        parent2.set_board(child2)
+        cross_over_solutions.append(parent1)
+        cross_over_solutions.append(parent2)
+
+    return cross_over_solutions
 
 
 # game course
@@ -96,17 +180,43 @@ def play_sudoku():
     solutions = generate_initial_solutions(input_board)  # generate 100 solutions
     f_sum, f_mean, f_max, f_min = set_fitness(solutions)  # calc the fitness of each solution
     best_fitness = gb.NUM_ROWS * 3  # the best score is 27
-    from_fitness_to_probs(solutions, f_sum)  # set the probability of each solution
-    solutions.sort(key=operator.attrgetter('prob'), reverse=True)  # sort instances by the probability
 
-    next_solutions = []
     # as long as there isn't a valid solution
+    generation = 0
     while f_max < best_fitness:
-        rep_sols = select_solutions_for_replications(solutions)  # get solutions from replications
+        from_fitness_to_probs(solutions, f_sum)  # set the probability of each solution
+        solutions.sort(key=operator.attrgetter('prob'), reverse=True)  # sort instances by the probability
+
+        rep_sols = bias_selection(solutions)  # get solutions from replications
         elit_sols = list(range(int(NUM_ELITISM)))  # get elite solutions
-        sols_indx_proceeding_next_gen = set(rep_sols + elit_sols)  # merge replication and elitism solutions
-        sols_proceeding_next_gen = [solutions[i] for i in sols_indx_proceeding_next_gen]  # get the solutions that stay as is for next generation
-        
+        sols_indx_proceeding_next_gen = list(set(rep_sols + elit_sols))  # get indices of replication and elitism solutions
+        # if the number of solutions achieved by replication and elitism is not even remove a replication solution
+        if len(sols_indx_proceeding_next_gen) % 2 != 0:
+            sols_indx_proceeding_next_gen = sols_indx_proceeding_next_gen[:-1]
+
+        replication_sol = [copy.deepcopy(solutions[i]) for i in sols_indx_proceeding_next_gen if i >= NUM_ELITISM]  # replication
+        elitism_sol = [copy.deepcopy(solutions[i]) for i in sols_indx_proceeding_next_gen if i < NUM_ELITISM]  # elitism
+        solutions_temp = copy.deepcopy(solutions)
+        cross_over_sol = cross_over(solutions_temp, int((NUM_SOL - len(sols_indx_proceeding_next_gen))/2))  # crossover
+
+        #  mutation
+        for sol in replication_sol + cross_over_sol:
+            sol.mutate()
+
+        del solutions  # delete the old solution list
+        solutions = replication_sol + elitism_sol + cross_over_sol  # create new solution list
+        f_sum, f_mean, f_max, f_min = set_fitness(solutions)  # calc the fitness of each solution
+
+        if generation % 100 == 0:
+            datetime_string = datetime.datetime.now().strftime("%y-%m-%d %H:%M:%S")
+            print(datetime_string + "\tgeneration: " + str(generation) + "\tmean fitness: " + str(f_min) +
+                  "\tmin fitness: " + str(f_min) + "\tmax fitness: " + str(f_max))
+
+        generation += 1
+
+    # print solution
+    for sol in solutions:
+        board = sol.get
 
 
 
